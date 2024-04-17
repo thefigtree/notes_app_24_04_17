e855d52b1d0f838de089809594324988e1b8cbc8
thefigtree <ldj8431@naver.com>
2024-04-17 21:52:25 +0900

    Login Ui

--- frontend/notes_app/src/pages/Login/Login.jsx
+++ frontend/notes_app/src/pages/Login/Login.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <>
       <Navbar></Navbar>
+
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={() => {}}>
+            <h4 className="text-2xl mb-7">Login</h4>
+            <input
+              type="text"
+              placeholder="Email"
+              className="input_box"
+            ></input>
+
+            <button type="submit" className="btn_primary">
+              Login
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              아직 가입을 안하셨나요?{" "}
+              <Link to="/signUp" className="font-medium text-primary underline">
+                계정을 만드시오.
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
--- frontend/notes_app/tailwind.config.js
+++ frontend/notes_app/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // 색상
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 };
