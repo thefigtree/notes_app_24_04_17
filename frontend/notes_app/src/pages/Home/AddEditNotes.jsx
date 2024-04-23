49b0e67b71e2f6864f1eed076238e68e82c3adbe
thefigtree <ldj8431@naver.com>
2024-04-23 20:26:23 +0900

    func: addNewNote

--- frontend/notes_app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes_app/src/pages/Home/AddEditNotes.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
 import TagInput from "../../components/Input/TagInput";
 import { MdClose } from "react-icons/md";
+import axiosInstance from "../../utils/axiosInstance";
 
-const AddEditNotes = ({ noteData, type, onClose }) => {
+const AddEditNotes = ({ noteData, type, getAllNotes, onClose }) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [tags, setTags] = useState([]);
   const [error, setError] = useState(null);
 
   //노트 추가하기
-  const addNewNote = async () => {};
+  const addNewNote = async () => {
+    try {
+      const response = await axiosInstance.post("/add-note", {
+        title,
+        content,
+        tags,
+      });
+
+      if (response.data && response.data.note) {
+        getAllNotes();
+        onClose();
+      }
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
 
   // 노트 제거하기
   const editNote = async () => {};
--- frontend/notes_app/src/pages/Home/Home.jsx
+++ frontend/notes_app/src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import Navbar from "../../components/Navbar";
 import NoteCard from "../../components/Card/NoteCard";
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
-// import Modal from "react-modal";
 import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
@@ -16,6 +15,7 @@ const Home = () => {
   });
 
   const [userInfo, setUserInfo] = useState(null);
+  const [allNotes, setAllNotes] = useState([]);
 
   const navigate = useNavigate();
 
@@ -35,8 +35,22 @@ const Home = () => {
     }
   };
 
+  // 기록한 모든 노트 가져오기
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-notes");
+
+      if (response.data && response.data.notes) {
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("An Unexpected Error Occured. Please Try Again.");
+    }
+  };
+
   useEffect(() => {
     getUserInfo();
+    getAllNotes();
     return () => {};
   }, []);
 
@@ -46,16 +60,19 @@ const Home = () => {
 
       <div className="container mx-auto">
         <div className="grid grid-cols-3 gap-4 mt-8">
-          <NoteCard
-            title="Meeting on 7th April"
-            date="3rd Apr 2024"
-            content="Meeting on 7th April Meeting on 7th April"
-            tags="#Meeting"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
-          ></NoteCard>
+          {allNotes.map((item, index) => (
+            <NoteCard
+              key={item._id}
+              title={item.title}
+              date={item.createdOn}
+              content={item.content}
+              tags={item.tags}
+              isPinned={item.isPinned}
+              onEdit={() => {}}
+              onDelete={() => {}}
+              onPinNote={() => {}}
+            ></NoteCard>
+          ))}
         </div>
       </div>
 
@@ -85,6 +102,7 @@ const Home = () => {
           onClose={() => {
             setOpenAddEditModal({ isShown: false, type: "add", data: null });
           }}
+          getAllNotes={getAllNotes}
         ></AddEditNotes>
       </Modal>
     </>
